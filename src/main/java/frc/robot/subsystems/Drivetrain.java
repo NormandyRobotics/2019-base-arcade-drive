98823e228f26a723b82b9557fd399490ac79c3a3
NormandyRobotics <89947633+NormandyRobotics@users.noreply.github.com>
2021-10-07 15:28:16 -0500

    first commit

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Drivetrain extends SubsystemBase {
+  PWMVictorSPX leftFrontMotor;
+  PWMVictorSPX leftRearMotor;
+  PWMVictorSPX rightFrontMotor;
+  PWMVictorSPX rightRearMotor;
+
+  SpeedControllerGroup leftMotors;
+  SpeedControllerGroup rightMotors;
+
+  DifferentialDrive driveMotors;
+  
+  
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    leftFrontMotor = new PWMVictorSPX(Constants.LEFT_FRONT_MOTOR);
+    leftFrontMotor.setInverted(true);
+    leftRearMotor = new PWMVictorSPX(Constants.LEFT_REAR_MOTOR);
+    leftRearMotor.setInverted(true);
+    rightFrontMotor = new PWMVictorSPX(Constants.RIGHT_FRONT_MOTOR);
+    rightFrontMotor.setInverted(false);
+    rightRearMotor = new PWMVictorSPX(Constants.RIGHT_REAR_MOTOR);
+    rightRearMotor.setInverted(false);
+
+    leftMotors = new SpeedControllerGroup(leftFrontMotor, leftRearMotor);
+    rightMotors = new SpeedControllerGroup(rightFrontMotor, rightRearMotor);
+
+    driveMotors = new DifferentialDrive(leftMotors, rightMotors);
+
+
+  }
+
+  public void driveWithJoysticks(){
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
