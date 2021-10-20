76552f01d98ea2176b3156dde6da62f4baf46201
NormandyRobotics <89947633+NormandyRobotics@users.noreply.github.com>
2021-10-20 09:33:41 -0500

    added new button commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,15 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.DriveWithJoysticks;
+import frc.robot.commands.RotateLeft;
+import frc.robot.commands.RotateRight;
 import frc.robot.commands.SpeedBoost;
 import frc.robot.commands.SpinInPlaceLeft;
+import frc.robot.commands.SpinInPlaceRight;
 import frc.robot.subsystems.Drivetrain;
 
 
@@ -28,13 +32,12 @@ public class RobotContainer {
   private final DriveWithJoysticks driveWithJoysticks;
 
 
-  
+  //initialize joysticks
+  public static XboxController driverJoystick;
+  public static XboxController operatorJoystick;
 
-
-
-    //initialize drive
-    public static XboxController driverJoystick;
-    public static XboxController operatorJoystick;
+  //Sendable chooser declare
+  SendableChooser<Command> chooser = new SendableChooser<>();
 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -58,41 +61,48 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-//create some buttons
-//final JoystickButton d_dpadUp = new JoystickButton(driverJoystick, Constants.D_DPAD_UP);
-//final JoystickButton d_dpadRight = new JoystickButton(driverJoystick, Constants.D_DPAD_RIGHT);
-//final JoystickButton d_dpadDown = new JoystickButton(driverJoystick, Constants.D_DPAD_DOWN);
-//final JoystickButton d_dpadLeft = new JoystickButton(driverJoystick, Constants.D_DPAD_LEFT);
-final JoystickButton d_l2 = new JoystickButton(driverJoystick, Constants.D_L2);
-final JoystickButton d_r2 = new JoystickButton(driverJoystick, Constants.D_R2);
-final JoystickButton d_l1 = new JoystickButton(driverJoystick, Constants.D_L1);
-final JoystickButton d_r1 = new JoystickButton(driverJoystick, Constants.D_R1);
-final JoystickButton d_joy1 = new JoystickButton(driverJoystick, Constants.D_JOY1);
-final JoystickButton d_joy2 = new JoystickButton(driverJoystick, Constants.D_JOY2);
-//final JoystickButton d_midLeft = new JoystickButton(driverJoystick, Constants.D_MIDLEFT);
-//final JoystickButton d_midRight = new JoystickButton(driverJoystick, Constants.D_MIDRIGHT);
+    //create some buttons
+    //final JoystickButton d_dpadUp = new JoystickButton(driverJoystick, Constants.D_DPAD_UP);
+    //final JoystickButton d_dpadRight = new JoystickButton(driverJoystick, Constants.D_DPAD_RIGHT);
+    //final JoystickButton d_dpadDown = new JoystickButton(driverJoystick, Constants.D_DPAD_DOWN);
+    //final JoystickButton d_dpadLeft = new JoystickButton(driverJoystick, Constants.D_DPAD_LEFT);
+    final JoystickButton d_l2 = new JoystickButton(driverJoystick, Constants.D_L2);
+    final JoystickButton d_r2 = new JoystickButton(driverJoystick, Constants.D_R2);
+    final JoystickButton d_l1 = new JoystickButton(driverJoystick, Constants.D_L1);
+    final JoystickButton d_r1 = new JoystickButton(driverJoystick, Constants.D_R1);
+    final JoystickButton d_joy1 = new JoystickButton(driverJoystick, Constants.D_JOY1);
+    final JoystickButton d_joy2 = new JoystickButton(driverJoystick, Constants.D_JOY2);
+    //final JoystickButton d_midLeft = new JoystickButton(driverJoystick, Constants.D_MIDLEFT);
+    //final JoystickButton d_midRight = new JoystickButton(driverJoystick, Constants.D_MIDRIGHT);
 
 
-//final JoystickButton o_dpadUp = new JoystickButton(operatorJoystick, Constants.O_DPAD_UP);
-//final JoystickButton o_dpadRight = new JoystickButton(operatorJoystick, Constants.O_DPAD_RIGHT);
-//final JoystickButton o_dpadDown = new JoystickButton(operatorJoystick, Constants.O_DPAD_DOWN);
-//final JoystickButton o_dpadLeft = new JoystickButton(operatorJoystick, Constants.O_DPAD_LEFT);
-//final JoystickButton o_l2 = new JoystickButton(operatorJoystick, Constants.O_L2);
-//final JoystickButton o_r2 = new JoystickButton(operatorJoystick, Constants.O_R2);
-//final JoystickButton o_l1 = new JoystickButton(operatorJoystick, Constants.O_L1);
-//final JoystickButton o_r1 = new JoystickButton(operatorJoystick, Constants.O_R1);
-//final JoystickButton o_joy1 = new JoystickButton(operatorJoystick, Constants.O_JOY1);
-//final JoystickButton o_joy2 = new JoystickButton(operatorJoystick, Constants.O_JOY2);
-//final JoystickButton o_midLeft = new JoystickButton(driverJoystick, Constants.O_MIDLEFT);
-//final JoystickButton o_midRight = new JoystickButton(driverJoystick, Constants.O_MIDRIGHT);
+    //final JoystickButton o_dpadUp = new JoystickButton(operatorJoystick, Constants.O_DPAD_UP);
+    //final JoystickButton o_dpadRight = new JoystickButton(operatorJoystick, Constants.O_DPAD_RIGHT);
+    //final JoystickButton o_dpadDown = new JoystickButton(operatorJoystick, Constants.O_DPAD_DOWN);
+    //final JoystickButton o_dpadLeft = new JoystickButton(operatorJoystick, Constants.O_DPAD_LEFT);
+    //final JoystickButton o_l2 = new JoystickButton(operatorJoystick, Constants.O_L2);
+    //final JoystickButton o_r2 = new JoystickButton(operatorJoystick, Constants.O_R2);
+    //final JoystickButton o_l1 = new JoystickButton(operatorJoystick, Constants.O_L1);
+    //final JoystickButton o_r1 = new JoystickButton(operatorJoystick, Constants.O_R1);
+    //final JoystickButton o_joy1 = new JoystickButton(operatorJoystick, Constants.O_JOY1);
+    //final JoystickButton o_joy2 = new JoystickButton(operatorJoystick, Constants.O_JOY2);
+    //final JoystickButton o_midLeft = new JoystickButton(driverJoystick, Constants.O_MIDLEFT);
+    //final JoystickButton o_midRight = new JoystickButton(driverJoystick, Constants.O_MIDRIGHT);
 
-//Speed boost when left joystick button held
-d_joy1.whenHeld(new SpeedBoost(driveTrain));
+    //Speed boost when left joystick button held
+    d_joy1.whenHeld(new SpeedBoost(driveTrain));
 
-//Spin in place to the left when top left trigger pressed
-d_l1.whenPressed(new SpinInPlaceLeft(driveTrain));
+    //Spin in place to the left when top left trigger pressed
+    d_l1.whenPressed(new SpinInPlaceLeft(driveTrain));
 
+    //Spin in place to the right when top right trigger pressed
+    d_r1.whenPressed(new SpinInPlaceRight(driveTrain));
 
+    //Rotate left when bottom left trigger pressed
+    d_l2.whenPressed(new RotateLeft(driveTrain));
+
+    //Rotate right when bottom right trigger pressed
+    d_r2.whenPressed(new RotateRight(driveTrain));
 
   }
 
@@ -104,9 +114,7 @@ d_l1.whenPressed(new SpinInPlaceLeft(driveTrain));
 
 
   public Command getAutonomousCommand() {
-    return driveWithJoysticks;
-    // An ExampleCommand will run in autonomous
-    //return m_autoCommand;
+    return chooser.getSelected();
   }
 
 }
